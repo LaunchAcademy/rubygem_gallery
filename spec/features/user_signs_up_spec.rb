341c1728e27668a082147f44dd9930f0a1ec5040
Peter Dunbar <Peter.joseph.dunbar@gmail.com>
2014-06-24 17:02:11 -0400

    added rspec tests for sign up (without matching passwords & without required criteria)

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -22,4 +22,38 @@ feature 'sign up', %Q{
     expect(page).to_not have_content("Sign Up")
 
   end
+
+  scenario 'and failing to confirm password matching' do
+    user = FactoryGirl.build(:user)
+
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'First Name', with: user.first_name #'Jon'
+    fill_in 'Last Name', with: user.last_name #'Smith'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: "drowssap"
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match ")
+
+
+  end
+
+  scenario 'and failing to enter required information' do
+    user = FactoryGirl.build(:user)
+
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: user.password
+    click_button 'Sign Up'
+
+    expect(page).to have_content("can't be blank")
+  end
+
+
+
+
 end
