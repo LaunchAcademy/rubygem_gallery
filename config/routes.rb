aa805a8ed9502ec557b3f25988c1398d38f4874e
Katie Blair <katieblair89@gmail.com>
2014-06-27 15:51:07 -0400

    Admin can view a list of all reviews and delete them--all tests passing.  Also fixed a typo in a previous spec.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'ruby_gems#index'
 
   namespace :admin do
-    resources :ruby_gems, :reviews
+    resources :ruby_gems
+    resources :reviews, only: [:index, :destroy]
   end
 end
