3947704987a6d632d195c53ded6f7905a424558a
Katie Blair <katieblair89@gmail.com>
2014-06-30 10:58:50 -0400

    Set up Carrierwave, no tests yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :profile_photo]
   end
 
   def authorized_to_edit?(review)
