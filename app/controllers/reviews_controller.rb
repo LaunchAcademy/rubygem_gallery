636e10b892963bc7ef61a63c42f4c0d7bd391985
Peter Dunbar <Peter.joseph.dunbar@gmail.com>
2014-07-02 11:32:47 -0400

    Added mandrill

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -50,7 +50,7 @@ class ReviewsController < ApplicationController
     @ruby_gem = @review.ruby_gem
     authorize_to_edit
     if @review.destroy
-      flash[:notice] = "Deleted"
+      flash[:notice] = "Deleted"y
       redirect_to ruby_gem_path(@ruby_gem)
     else
       flash[:notice] = "Error"
